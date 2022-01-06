ebfe86e179ed533fdafd695cfdedb61fee522d40
Ninel <inel2008@bk.ru>
2022-01-06 22:05:18 +0500

    WIP: add error handling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {Routes, Route, useNavigate} from 'react-router-dom';
 import {app} from './firebase-config';
 import {getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword} from 'firebase/auth';
 import {useEffect, useState} from 'react';
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
     const [email, setEmail] = useState('');
@@ -28,6 +30,14 @@ function App() {
                     navigate('/home')
                     sessionStorage.setItem('Auth Token', response._tokenResponse.refreshToken)
                 })
+                .catch((error) => {
+                    if (error.code === 'auth/wrong-password') {
+                        toast.error('Please check the Password');
+                    }
+                    if (error.code === 'auth/user-not-found') {
+                        toast.error('Please check the Email');
+                    }
+                })
         }
 
         if (id === 2) {
@@ -36,23 +46,35 @@ function App() {
                     navigate('/home');
                     sessionStorage.setItem('Auth Token', response._tokenResponse.refreshToken)
                 })
+                .catch((error) => {
+                    if (error.code === 'auth/wrong-password') {
+                        toast.error('Please check the Password');
+                    }
+                    if (error.code === 'auth/user-not-found') {
+                        toast.error('Please check the Email');
+                    }
+                    if (error.code === 'auth/email-already-in-use') {
+                        toast.error('Email Already in Use');
+                    }
+                })
         }
     }
 
     return (
-            <div className="App">
-                <>
-                    <Routes>
-                        <Route path='/login'
-                               element={<Form title="Login" setEmail={setEmail} setPassword={setPassword}
-                                              handleAction={() => handleAction(1)}/>}/>
-                        <Route path='/register'
-                               element={<Form title="Register" setEmail={setEmail}
-                                              setPassword={setPassword} handleAction={() => handleAction(2)}/>}/>
-                        <Route path='/home' element={<Home/>}/>
-                    </Routes>
-                </>
-            </div>
+        <div className="App">
+            <>
+                <ToastContainer/>
+                <Routes>
+                    <Route path='/login'
+                           element={<Form title="Login" setEmail={setEmail} setPassword={setPassword}
+                                          handleAction={() => handleAction(1)}/>}/>
+                    <Route path='/register'
+                           element={<Form title="Register" setEmail={setEmail}
+                                          setPassword={setPassword} handleAction={() => handleAction(2)}/>}/>
+                    <Route path='/home' element={<Home/>}/>
+                </Routes>
+            </>
+        </div>
     );
 }
 
