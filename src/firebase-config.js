77f8b6ecb61385832efeb3791f5d9deef0ce497b
Ninel <inel2008@bk.ru>
2022-01-06 21:43:44 +0500

    config: add firebase config

---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCOes-lcZy6b_RhasUEaJhnFl4AvNpibp4",
+    authDomain: "react-register-678fc.firebaseapp.com",
+    projectId: "react-register-678fc",
+    storageBucket: "react-register-678fc.appspot.com",
+    messagingSenderId: "335407765595",
+    appId: "1:335407765595:web:9874c95adc554323b8077e"
+};
+
+export const app = initializeApp(firebaseConfig);
